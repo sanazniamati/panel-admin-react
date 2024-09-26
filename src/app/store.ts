70186d5c456bb595959d2e4c039699e050dc648a
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-09-26 20:40:40 +0330

    added customerSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,21 @@
 /** @format */
 
 import { configureStore } from "@reduxjs/toolkit";
-import { authApi } from "../services/authApi";
+import { authApi } from "../services/authApi/authApi";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
-import authReducer from "../featchers/authSlice";
+import authReducer from "../featchers/auth/authSlice";
+import customerReducer from "../featchers/customer/customerSlice";
+import { customerApi } from "../services/customerApi/customerApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    customer: customerReducer,
+    [customerApi.reducerPath]: customerApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware).concat(customerApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
